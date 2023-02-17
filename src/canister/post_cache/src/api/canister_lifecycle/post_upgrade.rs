47d7a7b1bbfeab61152d3335ddcbe10015e417ba
Saikat Das <saikatdas0790@gmail.com>
2023-02-17 22:45:08 +0530

    refactor: add the remaining post_upgrage logic for deserialization using serde

--- src/canister/post_cache/src/api/canister_lifecycle/post_upgrade.rs
+++ src/canister/post_cache/src/api/canister_lifecycle/post_upgrade.rs
@@ -1,15 +1,22 @@
-use ic_cdk::storage;
+use shared_utils::common::utils::stable_memory_serializer_deserializer;
 
 use crate::{data_model::CanisterData, CANISTER_DATA};
 
+use super::pre_upgrade::BUFFER_SIZE_BYTES;
+
 #[ic_cdk_macros::post_upgrade]
 fn post_upgrade() {
-    match storage::stable_restore::<(CanisterData,)>() {
-        Ok((canister_data,)) => {
+    match stable_memory_serializer_deserializer::deserialize_from_stable_memory::<CanisterData>(
+        BUFFER_SIZE_BYTES,
+    ) {
+        Ok(canister_data) => {
             CANISTER_DATA.with(|canister_data_ref_cell| {
                 *canister_data_ref_cell.borrow_mut() = canister_data;
             });
         }
-        Err(_) => panic!("Failed to deserialize stable state"),
+        Err(e) => {
+            ic_cdk::print(format!("Error: {:?}", e));
+            panic!("Failed to restore canister data from stable memory");
+        }
     }
 }
