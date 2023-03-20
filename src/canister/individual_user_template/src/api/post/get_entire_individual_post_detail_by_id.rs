3f7034a1a03034a59f692dcd90bdb40786b7679c
Saikat Das <saikatdas0790@gmail.com>
2023-03-20 09:44:31 +0530

    chore: let super admin query all details of a post (#98)

---
+++ src/canister/individual_user_template/src/api/post/get_entire_individual_post_detail_by_id.rs
@@ -0,0 +1,36 @@
+use shared_utils::{
+    canister_specific::individual_user_template::types::post::Post,
+    common::types::known_principal::KnownPrincipalType,
+};
+
+use crate::CANISTER_DATA;
+
+#[ic_cdk::query]
+#[candid::candid_method(query)]
+pub fn get_entire_individual_post_detail_by_id(post_id: u64) -> Result<Post, ()> {
+    let api_caller = ic_cdk::caller();
+
+    let super_admin_user = CANISTER_DATA.with(|canister_data_ref_cell| {
+        canister_data_ref_cell
+            .borrow()
+            .known_principal_ids
+            .get(&KnownPrincipalType::UserIdGlobalSuperAdmin)
+            .unwrap()
+            .clone()
+    });
+
+    if api_caller != super_admin_user {
+        return Err(());
+    }
+
+    CANISTER_DATA.with(|canister_data_ref_cell| {
+        let post = canister_data_ref_cell
+            .borrow()
+            .all_created_posts
+            .get(&post_id)
+            .unwrap()
+            .clone();
+
+        Ok(post)
+    })
+}
