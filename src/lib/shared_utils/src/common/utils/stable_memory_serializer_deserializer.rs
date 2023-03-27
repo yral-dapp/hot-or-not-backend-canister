00f578fcdadaec2af4aa5a4c3bd8fd5f9eccdf16
Saikat Das <saikatdas0790@gmail.com>
2023-03-27 12:26:52 +0530

    refactor: import WASM page size from ic_cdk instead of specifying inline

--- src/lib/shared_utils/src/common/utils/stable_memory_serializer_deserializer.rs
+++ src/lib/shared_utils/src/common/utils/stable_memory_serializer_deserializer.rs
@@ -1,10 +1,10 @@
-use ic_cdk::api::stable::{BufferedStableReader, BufferedStableWriter};
+use ic_cdk::api::stable::{BufferedStableReader, BufferedStableWriter, WASM_PAGE_SIZE_IN_BYTES};
 use serde::{de::DeserializeOwned, Serialize};
 use std::cmp::min;
 use std::error::Error;
 use std::io::{Read, Write};
 
-const WASM_PAGE_SIZE_BYTES: usize = 64 * 1024; // 64KB
+// * const WASM_PAGE_SIZE_BYTES: usize = 64 * 1024; // 64KB
 
 pub fn serialize_to_stable_memory<S: Serialize>(
     state: S,
@@ -17,7 +17,7 @@ pub fn serialize_to_stable_memory<S: Serialize>(
 pub fn deserialize_from_stable_memory<S: DeserializeOwned>(
     max_buffer_size: usize,
 ) -> Result<S, impl Error> {
-    let stable_size = ic_cdk::api::stable::stable_size() as usize * WASM_PAGE_SIZE_BYTES;
+    let stable_size = ic_cdk::api::stable::stable_size() as usize * WASM_PAGE_SIZE_IN_BYTES;
     let buffer_size = min(max_buffer_size, stable_size);
     let reader = BufferedStableReader::new(buffer_size);
     deserialize(reader)
