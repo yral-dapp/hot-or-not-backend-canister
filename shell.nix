2950046ea950df031498f5dea3aa047dbfdacc9a
Saikat Das <saikatdas0790@gmail.com>
2023-03-31 21:03:46 +0530

    build: include installing rust stable and adding wasm32 target when initializing nix-shell

--- shell.nix
+++ shell.nix
@@ -2,8 +2,12 @@
 let
   dfx-env = import (fetchTarball https://github.com/ninegua/ic-nix/releases/latest/download/dfx-env.tar.gz) { version = "20230330"; };
 in
-# TODO: modify to include installing rust stable and adding wasm32 target
 dfx-env.overrideAttrs (old: {
   nativeBuildInputs = with pkgs; old.nativeBuildInputs ++
     [ rustup pkg-config openssl protobuf cmake cachix killall ];
+
+  shellHook = ''
+    rustup toolchain install stable
+    rustup target add wasm32-unknown-unknown
+  '';
 })
