bcd205fcbc9a72f5f490433165c216a1b667ada7
Saikat Das <saikatdas0790@gmail.com>
2023-03-31 19:02:33 +0530

    test: update ic_repl tests to point to the new user_index canister binding

--- ic_repl_tests/individual_user_template/post/post.test.sh
+++ ic_repl_tests/individual_user_template/post/post.test.sh
@@ -1,4 +1,4 @@
-import user_index_canister = "rkp4c-7iaaa-aaaaa-aaaca-cai";
+import user_index_canister = "br5f7-7uaaa-aaaaa-qaaca-cai";
 identity default;
 
 let my_canister = call user_index_canister.get_requester_principals_canister_id_create_if_not_exists_and_optionally_allow_referrer(null);
