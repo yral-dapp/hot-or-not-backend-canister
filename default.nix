659026493720b0357584287849c7367b6b197dee
Saikat Das <saikatdas0790@gmail.com>
2023-06-13 14:51:05 +0200

    feat: complete script for local upgrades when SNS'ed

--- default.nix
+++ default.nix
@@ -3,11 +3,11 @@ let
   rev = "cc45a3f8c98e1c33ca996e3504adefbf660a72d1";
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
   pkgs = import nixpkgs { };
-  dfx-env = import (builtins.fetchTarball "https://github.com/ninegua/ic-nix/releases/latest/download/dfx-env.tar.gz") { version = "20230508"; inherit pkgs; };
+  dfx-env = import (builtins.fetchTarball "https://github.com/ninegua/ic-nix/releases/latest/download/dfx-env.tar.gz") { version = "20230605"; inherit pkgs; };
 in
 dfx-env.overrideAttrs (old: {
   nativeBuildInputs = with pkgs; old.nativeBuildInputs ++
-    [ rustup pkg-config openssl protobuf cmake cachix killall ];
+    [ rustup pkg-config openssl protobuf cmake cachix killall jq coreutils bc ];
 
   shellHook = ''
     rustup toolchain install stable
