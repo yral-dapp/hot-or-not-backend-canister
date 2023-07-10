0e83bec6507c494d9ce6156c4778922f940bfc5d
Saikat Das <saikatdas0790@gmail.com>
2023-07-10 16:21:00 +0200

    refactor: cleanup leftover canister settings (#175)

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ let
   rev = "cc45a3f8c98e1c33ca996e3504adefbf660a72d1";
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
   pkgs = import nixpkgs { };
-  dfx-env = import (builtins.fetchTarball "https://github.com/ninegua/ic-nix/releases/latest/download/dfx-env.tar.gz") { version = "20230605"; inherit pkgs; };
+  dfx-env = import (builtins.fetchTarball "https://github.com/ninegua/ic-nix/releases/latest/download/dfx-env.tar.gz") { version = "20230704"; inherit pkgs; };
 in
 dfx-env.overrideAttrs (old: {
   nativeBuildInputs = with pkgs; old.nativeBuildInputs ++
