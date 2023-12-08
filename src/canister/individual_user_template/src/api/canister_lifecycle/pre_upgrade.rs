951f0ab2f6c0b2020c305c457cf129b45aa672e5
Ravi Sawlani <152961362+ravibazz@users.noreply.github.com>
2023-12-08 10:19:04 +0530

    migrate individual template data stage 1 (#198)
    * add memory manager and update pre_upgrade hook
    
    * remove send_metrices from post_upgrade; update recharge amout in user_index
    
    * fix test

--- src/canister/individual_user_template/src/api/canister_lifecycle/pre_upgrade.rs
+++ src/canister/individual_user_template/src/api/canister_lifecycle/pre_upgrade.rs
@@ -1,17 +1,22 @@
-use shared_utils::common::utils::stable_memory_serializer_deserializer;
+use ciborium::ser;
+use ic_stable_structures::writer::Writer;
 
+use crate::data_model::memory;
 use crate::CANISTER_DATA;
 
 pub const BUFFER_SIZE_BYTES: usize = 2 * 1024 * 1024; // 2 MiB
 
 #[ic_cdk::pre_upgrade]
 fn pre_upgrade() {
+    let mut state_bytes = vec![];
     CANISTER_DATA.with(|canister_data_ref_cell| {
-        let canister_data = canister_data_ref_cell.take();
-        stable_memory_serializer_deserializer::serialize_to_stable_memory(
-            canister_data,
-            BUFFER_SIZE_BYTES,
-        )
-        .expect("Failed to serialize canister data");
-    });
+        ser::into_writer(&*canister_data_ref_cell.borrow(), &mut state_bytes)
+    })
+    .expect("failed to encode state");
+
+    let len = state_bytes.len() as u32;
+    let mut memory = memory::get_upgrades_memory();
+    let mut writer = Writer::new(&mut memory, 0);
+    writer.write(&len.to_le_bytes()).unwrap();
+    writer.write(&state_bytes).unwrap();
 }
