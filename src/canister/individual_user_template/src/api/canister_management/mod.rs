86533e35eb40108dec170e816e1679583090fad0
Ravi Sawlani <ravi@gobazzinga.io>
2023-12-28 21:53:40 +0530

    added version_number query method for individual_user_template canister

--- src/canister/individual_user_template/src/api/canister_management/mod.rs
+++ src/canister/individual_user_template/src/api/canister_management/mod.rs
@@ -1,7 +1,18 @@
 use ic_cdk::api::stable::stable_size;
 
+use crate::CANISTER_DATA;
+
 #[candid::candid_method(query)]
 #[ic_cdk::query]
 pub fn get_stable_memory_size() -> u32 {
     stable_size()
+}
+
+
+#[candid::candid_method(query)]
+#[ic_cdk::query]
+pub fn get_version_number() -> u64 {
+    CANISTER_DATA.with(|canister_data_ref| {
+        canister_data_ref.borrow().version_details.version_number
+    })
 }
