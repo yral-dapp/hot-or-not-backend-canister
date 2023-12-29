f3450358aa15ec6584c594864435c211e2bcfe1c
Ravi Sawlani <ravi@gobazzinga.io>
2023-12-29 16:34:45 +0530

    do not drop stable memory on upgrade

--- src/canister/user_index/src/api/upgrade_individual_user_template/update_user_index_upgrade_user_canisters_with_latest_wasm.rs
+++ src/canister/user_index/src/api/upgrade_individual_user_template/update_user_index_upgrade_user_canisters_with_latest_wasm.rs
@@ -146,7 +146,7 @@ async fn upgrade_user_canister(
                 configuration.url_to_send_canister_metrics_to.clone(),
             ),
         },
-        true
+        false
     )
     .await
     .map_err(|e| e.1)
