08db52ef2fb624ee124f01f7e1615949564c4f88
Ravi Sawlani <152961362+ravibazz@users.noreply.github.com>
2024-01-10 20:02:07 +0530

    fix: repl test to include nsfw field in post (#226)

--- ic_repl_tests/individual_user_template/post/post_toggle_like_check_liked_by_me.test.sh
+++ ic_repl_tests/individual_user_template/post/post_toggle_like_check_liked_by_me.test.sh
@@ -4,7 +4,7 @@ identity default;
 
 let my_canister = call user_index_canister.get_requester_principals_canister_id_create_if_not_exists_and_optionally_allow_referrer(null);
 
-let result = call my_canister.add_post_v2(record { hashtags = vec { "a"; "b"; "c" }; description = "This is post from integration test"; video_uid = "#1234567890"; creator_consent_for_inclusion_in_hot_or_not = false; });
+let result = call my_canister.add_post_v2(record { hashtags = vec { "a"; "b"; "c" }; description = "This is post from integration test"; video_uid = "#1234567890"; creator_consent_for_inclusion_in_hot_or_not = false; is_nsfw = false});
 let post_id = result.Ok;
 
 call my_canister.update_post_toggle_like_status_by_caller(post_id);
