b9fabf1e46ce289e85c44c680543d450a3960b9c
komal-rs <komalsai@gobazzinga.io>
2024-02-08 21:04:36 +0530

    added new fields StableBtreeMap

--- src/canister/individual_user_template/src/data_model/memory.rs
+++ src/canister/individual_user_template/src/data_model/memory.rs
@@ -1,4 +1,7 @@
-use ic_stable_structures::{DefaultMemoryImpl, memory_manager::{MemoryId, VirtualMemory, MemoryManager}};
+use ic_stable_structures::{
+    memory_manager::{MemoryId, MemoryManager, VirtualMemory},
+    DefaultMemoryImpl,
+};
 use std::cell::RefCell;
 
 // A memory for upgrades, where data from the heap can be serialized/deserialized.
@@ -7,6 +10,8 @@ const UPGRADES: MemoryId = MemoryId::new(0);
 // A memory for the StableBTreeMap we're using. A new memory should be created for
 // every additional stable structure.
 
+const ROOM_DETAILS_MEMORY: MemoryId = MemoryId::new(1);
+const BET_DETAILS_MEMORY: MemoryId = MemoryId::new(2);
 
 pub type Memory = VirtualMemory<DefaultMemoryImpl>;
 
@@ -19,6 +24,14 @@ thread_local! {
 
 pub fn get_upgrades_memory() -> Memory {
     MEMORY_MANAGER.with(|m| m.borrow_mut().get(UPGRADES))
+}
+
+pub fn get_room_details_memory() -> Memory {
+    MEMORY_MANAGER.with(|m| m.borrow_mut().get(ROOM_DETAILS_MEMORY))
+}
+
+pub fn get_bet_details_memory() -> Memory {
+    MEMORY_MANAGER.with(|m| m.borrow_mut().get(BET_DETAILS_MEMORY))
 }
 
 pub fn init_memory_manager() {
