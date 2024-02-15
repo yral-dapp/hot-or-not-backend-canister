17047b1fee8a34f0beb36e3dd30fa8b05851410c
komal-rs <komalsai@gobazzinga.io>
2024-02-15 15:10:22 +0530

    auth for snapshot APIs

--- src/canister/individual_user_template/src/api/snapshot/get_snapshot.rs
+++ src/canister/individual_user_template/src/api/snapshot/get_snapshot.rs
@@ -5,10 +5,25 @@ use crate::{
     },
     CANISTER_DATA,
 };
+use candid::Principal;
 use ic_cdk::api::stable;
 use ic_stable_structures::{memory_manager::MemoryId, writer::Writer, Memory};
+use shared_utils::constant::RECLAIM_CANISTER_PRINCIPAL_ID;
 
-#[ic_cdk::update]
+pub fn is_reclaim_canister_id() -> Result<(), String> {
+    let caller = ic_cdk::caller();
+    let reclaim_canister_principal = Principal::from_text(RECLAIM_CANISTER_PRINCIPAL_ID).unwrap();
+
+    // Here accessing the args ???
+
+    if caller == reclaim_canister_principal {
+        Ok(())
+    } else {
+        Err("Caller is not allowed.".to_string())
+    }
+}
+
+#[ic_cdk::update(guard = "is_reclaim_canister_id")]
 #[candid::candid_method(update)]
 fn save_snapshot_json() -> u32 {
     let mut state_bytes = vec![];
@@ -40,7 +55,7 @@ fn save_snapshot_json() -> u32 {
     len
 }
 
-#[ic_cdk::query]
+#[ic_cdk::query(guard = "is_reclaim_canister_id")]
 #[candid::candid_method(query)]
 fn download_snapshot(offset: u64, length: u64) -> Vec<u8> {
     let snapshot_memory = MEMORY_MANAGER.with(|m| m.borrow_mut().get(MemoryId::new(5)));
@@ -52,7 +67,7 @@ fn download_snapshot(offset: u64, length: u64) -> Vec<u8> {
     state_bytes
 }
 
-#[ic_cdk::update]
+#[ic_cdk::update(guard = "is_reclaim_canister_id")]
 #[candid::candid_method(update)]
 fn receive_and_save_snaphot(offset: u64, state_bytes: Vec<u8>) {
     if stable::stable_size() == 0 {
@@ -64,7 +79,7 @@ fn receive_and_save_snaphot(offset: u64, state_bytes: Vec<u8>) {
     writer.write(&state_bytes).unwrap();
 }
 
-#[ic_cdk::update]
+#[ic_cdk::update(guard = "is_reclaim_canister_id")]
 #[candid::candid_method(update)]
 fn load_snapshot(length: u64) {
     let snapshot_memory = get_snapshot_memory();
