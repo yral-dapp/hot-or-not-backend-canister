4d5490e1144670c936f4db0779a0880dc41d02c7
ravibazz <ravi@gobazzinga.io>
2024-02-20 18:48:52 +0530

    fix: save version args in post upgrade in plaform orchestrator

--- src/canister/platform_orchestrator/src/api/canister_lifecycle/post_upgrade.rs
+++ src/canister/platform_orchestrator/src/api/canister_lifecycle/post_upgrade.rs
@@ -1,6 +1,7 @@
 use ciborium::de;
 use ic_cdk_macros::post_upgrade;
 use ic_stable_structures::Memory;
+use shared_utils::{canister_specific::platform_orchestrator::types::args::PlatformOrchestratorInitArgs, common::utils::system_time};
 
 use crate::{data_model::memory, CANISTER_DATA};
 
@@ -9,6 +10,11 @@ use crate::{data_model::memory, CANISTER_DATA};
 
 #[post_upgrade]
 pub fn post_upgrade() {
+   restore_data_from_stable_memory();
+   update_version_from_args();
+}
+
+fn restore_data_from_stable_memory() {
    let heap_data = memory::get_upgrades_memory();
    let mut heap_data_len_bytes = [0; 4];
    heap_data.read(0, &mut heap_data_len_bytes);
@@ -19,5 +25,13 @@ pub fn post_upgrade() {
    let canister_data = de::from_reader(&*canister_data_bytes).expect("Failed to deserialize heap data");
    CANISTER_DATA.with_borrow_mut(|cd| {
         *cd = canister_data;
+   })
+}
+
+fn update_version_from_args() {
+   let (upgrade_args,) = ic_cdk::api::call::arg_data::<(PlatformOrchestratorInitArgs,)>();
+   CANISTER_DATA.with_borrow_mut(|canister_data| {
+      canister_data.version_detail.version = upgrade_args.version;
+      canister_data.version_detail.last_update_on = system_time::get_current_system_time();
    })
 }
