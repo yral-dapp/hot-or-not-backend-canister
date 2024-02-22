59a8e29980a77181b05298cbb448943b2da28146
ravibazz <ravi@gobazzinga.io>
2024-02-22 17:08:51 +0530

    compress all the wasm files in install script

--- scripts/canisters/local_deploy/install_all_canisters.sh
+++ scripts/canisters/local_deploy/install_all_canisters.sh
@@ -29,10 +29,15 @@ dfx canister create --no-wallet platform_orchestrator
 dfx build individual_user_template
 gzip -f -1 ./target/wasm32-unknown-unknown/release/individual_user_template.wasm
 dfx build configuration
+gzip -f -1 ./target/wasm32-unknown-unknown/release/configuration.wasm
 dfx build data_backup
+gzip -f -1 ./target/wasm32-unknown-unknown/release/data_backup.wasm
 dfx build user_index
+gzip -f -1 ./target/wasm32-unknown-unknown/release/user_index.wasm
 dfx build post_cache
+gzip -f -1 ./target/wasm32-unknown-unknown/release/post_cache.wasm
 dfx build platform_orchestrator
+gzip -f -1 ./target/wasm32-unknown-unknown/release/platform_orchestrator.wasm
 
 if [[ $skip_test != true ]]
 then
