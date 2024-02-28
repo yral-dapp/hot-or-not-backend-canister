6bdbdf50fbc97c7bbf96bff13786641c8a0079c8
ravibazz <ravi@gobazzinga.io>
2024-02-28 21:38:21 +0530

    update create_pool to provision canisters concurrently

--- src/canister/user_index/src/api/canister_management/create_pool_of_available_canisters.rs
+++ src/canister/user_index/src/api/canister_management/create_pool_of_available_canisters.rs
@@ -1,9 +1,16 @@
+use std::pin::Pin;
+
+use futures::Future;
 use ic_cdk::{api::is_controller, caller};
 use shared_utils::{common::{types::wasm::{CanisterWasm, WasmType}, utils::task::run_task_concurrently}, constant::{BACKUP_INDIVIDUAL_USER_CANISTER_BATCH_SIZE, INDIVIDUAL_USER_CANISTER_SUBNET_BATCH_SIZE}};
 use ic_cdk_macros::update;
 
 use crate::{util::canister_management::{create_empty_user_canister, create_users_canister}, CANISTER_DATA};
 
+enum CanisterCodeState {
+    Empty,
+    WasmInstalled
+}
 
 #[update]
 pub fn create_pool_of_individual_user_available_canisters(version: String, individual_user_wasm: Vec<u8>) -> Result<String, String> {
@@ -26,20 +33,33 @@ pub fn create_pool_of_individual_user_available_canisters(version: String, indiv
 
 
 pub async fn impl_create_pool_of_individual_user_available_canisters(version: String, individual_user_wasm: Vec<u8>) {
-    
-    //Canisters with installed wasm
-    for _ in 0..INDIVIDUAL_USER_CANISTER_SUBNET_BATCH_SIZE {
-        let canister_id = create_users_canister(None, version.clone(), individual_user_wasm.clone()).await;
-        CANISTER_DATA.with_borrow_mut(|canister_data| {
-            canister_data.available_canisters.insert(canister_id);
-        })
-    }
 
-    //Empty backup canisters
-    for _ in 0 .. BACKUP_INDIVIDUAL_USER_CANISTER_BATCH_SIZE {
+    //empty canister for backup
+    let create_empty_canister_futures = (0..BACKUP_INDIVIDUAL_USER_CANISTER_BATCH_SIZE)
+    .map(|_| Box::pin(async {
         let canister_id = create_empty_user_canister().await;
+        (canister_id, CanisterCodeState::Empty)
+    }) as Pin<Box<dyn Future<Output = _>>>);
+    
+    //canisters with installed wasm for available pool
+    let create_canister_with_wasm_futures = (0..INDIVIDUAL_USER_CANISTER_SUBNET_BATCH_SIZE)
+    .map(|_| Box::pin(async {
+        let canister_id = create_users_canister(None, version.clone(), individual_user_wasm.clone()).await;
+        (canister_id, CanisterCodeState::WasmInstalled)
+    }) as Pin<Box<dyn Future<Output = _>>>);
+
+    let combined_create_canister_futures = create_canister_with_wasm_futures.chain(create_empty_canister_futures);
+
+    run_task_concurrently(combined_create_canister_futures,
+    10,
+    |(canister_id, canister_code_state)| {
         CANISTER_DATA.with_borrow_mut(|canister_data| {
-            canister_data.backup_canister_pool.insert(canister_id);
-        })
-    }
+            match canister_code_state {
+                CanisterCodeState::Empty => canister_data.backup_canister_pool.insert(canister_id),
+                CanisterCodeState::WasmInstalled => canister_data.available_canisters.insert(canister_id)
+            };
+        });
+    }, 
+    || false).await;
+
 }
