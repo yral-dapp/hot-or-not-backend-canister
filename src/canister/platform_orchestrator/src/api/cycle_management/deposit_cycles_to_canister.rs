fd1ac568f3a4129cf66a2ba9c9338f3ef4c57c34
ravibazz <ravi@gobazzinga.io>
2024-03-06 17:19:14 +0530

    added method to upgrade specific individual canister from platform orchestrator

---
+++ src/canister/platform_orchestrator/src/api/cycle_management/deposit_cycles_to_canister.rs
@@ -0,0 +1,13 @@
+use candid::Principal;
+use ic_cdk::{api::management_canister::main::{deposit_cycles, CanisterIdRecord}, update};
+use shared_utils::common::utils::permissions::is_caller_controller;
+
+
+#[update(guard= "is_caller_controller")]
+async fn deposit_cycles_to_canister(canister_id: Principal, cycles: u128) -> Result<String, String> {
+    deposit_cycles(CanisterIdRecord {canister_id}, cycles)
+    .await
+    .map_err(|e| e.1)?;
+
+    Ok(String::from("Success"))
+}
