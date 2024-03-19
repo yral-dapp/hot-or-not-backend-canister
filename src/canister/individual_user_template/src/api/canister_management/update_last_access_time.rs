2e7d770e2c0918b4eb03999bb6c16e8970183105
ravibazz <ravi@gobazzinga.io>
2024-03-19 09:11:37 +0530

    update guard from global admin to profile owner for last access time

--- src/canister/individual_user_template/src/api/canister_management/update_last_access_time.rs
+++ src/canister/individual_user_template/src/api/canister_management/update_last_access_time.rs
@@ -1,12 +1,18 @@
 
+use ic_cdk::caller;
 use ic_cdk_macros::update;
 use shared_utils::{canister_specific::individual_user_template::types::session::SessionType, common::utils::permissions::is_caller_global_admin};
 use shared_utils::common::utils::system_time::get_current_system_time_from_ic;
 
 use crate::CANISTER_DATA;
 
-#[update(guard="is_caller_global_admin")]
+#[update]
 fn update_last_access_time() -> Result<String, String> {
+
+    let profile_owner = CANISTER_DATA.with_borrow(|canister_data| canister_data.profile.principal_id.unwrap());
+    if profile_owner != caller() {
+        return Err("Unauthorized".into())
+    }
     
     CANISTER_DATA.with_borrow_mut(|canister_data| {
         let session_type = canister_data.session_type.ok_or(String::from("User is not yet registered"))?;
