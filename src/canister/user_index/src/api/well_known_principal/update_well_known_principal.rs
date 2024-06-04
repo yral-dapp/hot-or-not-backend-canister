2cc23d304553771b017d8837da9b1c0375f6f83c
Ravi Sawlani <152961362+ravibazz@users.noreply.github.com>
2024-06-05 02:56:18 +0530

    fixes update global known principal (#316)
    * fix update_global_known_principal
    
    * ignore migration test
    
    * Revert "ignore migration test"
    
    This reverts commit 98809dfc2d87d5952952e63dad57e49f334c8c95.

--- src/canister/user_index/src/api/well_known_principal/update_well_known_principal.rs
+++ src/canister/user_index/src/api/well_known_principal/update_well_known_principal.rs
@@ -1,31 +1,100 @@
+use std::borrow::BorrowMut;
+
 use candid::Principal;
-use ic_cdk::notify;
+use ic_cdk::{call, notify};
 use ic_cdk_macros::update;
-use shared_utils::common::{types::known_principal::KnownPrincipalType, utils::{permissions::is_caller_controller, task::run_task_concurrently}};
+use shared_utils::{
+    canister_specific::user_index::types::BroadcastCallStatus,
+    common::{
+        types::known_principal::KnownPrincipalType,
+        utils::{
+            permissions::is_caller_controller, system_time::get_current_system_time,
+            task::run_task_concurrently,
+        },
+    },
+};
 
 use crate::CANISTER_DATA;
 
-
-#[update(guard="is_caller_controller")]
+#[update(guard = "is_caller_controller")]
 fn update_well_known_principal(known_principal_type: KnownPrincipalType, value: Principal) {
     CANISTER_DATA.with_borrow_mut(|canister_data| {
-        canister_data.configuration.known_principal_ids.insert(known_principal_type, value);
+        canister_data
+            .configuration
+            .known_principal_ids
+            .insert(known_principal_type, value);
     });
 
-    issue_update_known_principal_for_individual_canisters(known_principal_type.clone(), value);
+    ic_cdk::spawn(issue_update_known_principal_for_individual_canisters(
+        known_principal_type,
+        value,
+    ));
 }
 
-
-fn issue_update_known_principal_for_individual_canisters(known_principal_type: KnownPrincipalType, value: Principal) {
+async fn issue_update_known_principal_for_individual_canisters(
+    known_principal_type: KnownPrincipalType,
+    value: Principal,
+) {
     let all_canisters = CANISTER_DATA.with_borrow(|canister_data| {
-        let mut all_canisters: Vec<Principal> = canister_data.user_principal_id_to_canister_id_map.values().copied().collect();
-        let mut available_canisters: Vec<Principal> = canister_data.available_canisters.iter().copied().collect();
-        all_canisters.extend(available_canisters.drain(..));
+        let mut all_canisters: Vec<Principal> = canister_data
+            .user_principal_id_to_canister_id_map
+            .values()
+            .copied()
+            .collect();
+        let mut available_canisters: Vec<Principal> =
+            canister_data.available_canisters.iter().copied().collect();
+        all_canisters.append(&mut available_canisters);
         all_canisters
-    }); 
-
-
-    all_canisters.iter().for_each(|individual_canister_id| {
-        let _ = notify(*individual_canister_id, "update_well_known_principal", (known_principal_type.clone(), value));
     });
+
+    let futures = all_canisters.iter().map(|individual_canister| async {
+        let res = call::<_, ()>(
+            *individual_canister,
+            "update_well_known_principal",
+            (known_principal_type, value),
+        )
+        .await;
+
+        match res {
+            Ok(()) => Ok(*individual_canister),
+            Err(e) => Err((*individual_canister, e.1)),
+        }
+    });
+
+    CANISTER_DATA.with_borrow_mut(|canister_data| {
+        canister_data.last_broadcast_call_status = BroadcastCallStatus {
+            method_name: "update_well_known_principal".into(),
+            timestamp: get_current_system_time(),
+            ..Default::default()
+        }
+    });
+
+    let result_callback = |res| {
+        CANISTER_DATA.with_borrow_mut(|canister_data| {
+            match res {
+                Ok(canister_id) => {
+                    canister_data
+                        .last_broadcast_call_status
+                        .successful_canister_ids
+                        .push(canister_id);
+
+                    canister_data
+                        .last_broadcast_call_status
+                        .successful_canisters_count += 1;
+                }
+                Err(e) => {
+                    canister_data
+                        .last_broadcast_call_status
+                        .failed_canister_ids
+                        .push(e);
+                    canister_data
+                        .last_broadcast_call_status
+                        .failed_canisters_count += 1;
+                }
+            }
+            canister_data.last_broadcast_call_status.total_canisters += 1;
+        })
+    };
+
+    run_task_concurrently(futures, 10, result_callback, || false).await;
 }
--- src/lib/shared_utils/src/canister_specific/user_index/types/mod.rs
+++ src/lib/shared_utils/src/canister_specific/user_index/types/mod.rs
@@ -1,4 +1,4 @@
-use std::time::SystemTime;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 use candid::{CandidType, Deserialize, Principal};
 use serde::Serialize;
@@ -12,4 +12,29 @@ pub struct RecycleStatus {
     pub last_recycled_at: Option<SystemTime>,
     pub last_recycled_duration: Option<u64>,
     pub failed_recycling: Vec<(Principal, String)>,
+}
+
+#[derive(Debug, CandidType, Serialize, Deserialize, Clone)]
+pub struct BroadcastCallStatus {
+    pub method_name: String,
+    pub successful_canister_ids: Vec<Principal>,
+    pub failed_canister_ids: Vec<(Principal, String)>,
+    pub successful_canisters_count: u64,
+    pub failed_canisters_count: u64,
+    pub total_canisters: u64,
+    pub timestamp: SystemTime,
+}
+
+impl Default for BroadcastCallStatus {
+    fn default() -> Self {
+        Self {
+            timestamp: UNIX_EPOCH,
+            successful_canister_ids: Vec::new(),
+            failed_canister_ids: Vec::new(),
+            method_name: String::from(""),
+            successful_canisters_count: 0,
+            total_canisters: 0,
+            failed_canisters_count: 0,
+        }
+    }
 }
