eb8a5e879eff0830a37fe07216243cbcf91d966b
abhishek <abhishektripathi@gobazzinga.io>
2024-06-27 15:56:48 +0530

    fix: include yral feed in mod.rs
    - to generate candid files for new endpoints

--- src/canister/post_cache/src/api/mod.rs
+++ src/canister/post_cache/src/api/mod.rs
@@ -2,6 +2,7 @@ pub mod canister_lifecycle;
 pub mod canister_management;
 pub mod feed;
 pub mod home_feed;
+pub mod yral_feed;
 pub mod hot_or_not_feed;
 pub mod monitoring;
 pub mod well_known_principal;
