9dca9fd71ae918cf103d9afd823ad66f5d4a2130
abhishek <abhishektripathi@gobazzinga.io>
2024-07-03 10:02:25 +0530

    yral game simulation test

--- src/lib/integration_tests/tests/hot_or_not/main.rs
+++ src/lib/integration_tests/tests/hot_or_not/main.rs
@@ -1,4 +1,5 @@
 pub mod hot_or_not_timely_index_update_test;
 pub mod hotornot_game_simulation_test;
+pub mod yral_game_simulation_test;
 pub mod reconcile_scores_test;
 pub mod when_bob_charlie_dan_place_bet_on_alice_created_post_then_expected_outcomes_occur;
