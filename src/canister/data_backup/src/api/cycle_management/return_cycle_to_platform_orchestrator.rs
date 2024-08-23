c475b7b2ee45f2c02ae81508b8d042b609f0b240
adarsh <adarsh@gobazzinga.io>
2024-08-23 14:17:20 +0530

    Hardcode principal of orchestrator canistor to send cycle
    Hardcoded the orchestrator canister principal in cycle return api in data backup and configuration canister

--- src/canister/data_backup/src/api/cycle_management/return_cycle_to_platform_orchestrator.rs
+++ src/canister/data_backup/src/api/cycle_management/return_cycle_to_platform_orchestrator.rs
@@ -1,22 +1,13 @@
+use candid::Principal;
 use ic_cdk::api::{
     canister_balance128,
     management_canister::{main, provisional::CanisterIdRecord},
 };
-use shared_utils::common::types::known_principal::KnownPrincipalType;
-
-use crate::CANISTER_DATA;
+use std::str::FromStr;
 
 #[ic_cdk::update]
 async fn return_cycles_to_platform_orchestrator_canister() {
-    let platform_orchestrator_canister_id = CANISTER_DATA.with(|canister_data_ref_cell| {
-        canister_data_ref_cell
-            .borrow()
-            .heap_data
-            .known_principal_ids
-            .get(&KnownPrincipalType::CanisterIdPlatformOrchestrator)
-            .cloned()
-            .unwrap()
-    });
+    let platform_orchestrator_canister_id = Principal::from_str("74zq4-iqaaa-aaaam-ab53a-cai").unwrap();
 
     if canister_balance128() > 200_000_000_000 {
         let cycle_amount_to_transfer = canister_balance128() - 200_000_000_000;
