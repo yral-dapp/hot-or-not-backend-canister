3085c17fb4247df87575f4b12bfad3f30b45cf49
adarsh <adarsh@gobazzinga.io>
2024-08-27 12:18:48 +0530

    Remove Databackup related stuffs from shared utils

--- src/lib/shared_utils/src/canister_specific/data_backup/types/backup_statistics.rs
+++
@@ -1,6 +0,0 @@
-use candid::{CandidType, Deserialize};
-
-#[derive(CandidType, Deserialize)]
-pub struct BackupStatistics {
-    pub number_of_user_entries: u64,
-}
--- src/lib/shared_utils/src/canister_specific/mod.rs
+++ src/lib/shared_utils/src/canister_specific/mod.rs
@@ -1,5 +1,4 @@
 pub mod configuration;
-pub mod data_backup;
 pub mod individual_user_template;
 pub mod post_cache;
 pub mod user_index;
