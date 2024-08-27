ea6617a96f40c120113825d6aa2fc87442924a6a
adarsh <adarsh@gobazzinga.io>
2024-08-27 15:56:54 +0530

    Update shared utils by removing configuration canister

--- src/lib/shared_utils/src/canister_specific/configuration/types/args.rs
+++
@@ -1,9 +0,0 @@
-use candid::{CandidType, Deserialize};
-
-use crate::common::types::known_principal::KnownPrincipalMap;
-
-#[derive(Deserialize, CandidType, Default)]
-pub struct ConfigurationInitArgs {
-    pub known_principal_ids: Option<KnownPrincipalMap>,
-    pub signups_enabled: Option<bool>,
-}
