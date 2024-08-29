0e2c7d242df2d66e7dab423edd66f01d3b6ba6f0
abhishek <abhishektripathi@gobazzinga.io>
2024-08-29 20:15:55 +0530

    fix: build

--- src/canister/individual_user_template/src/api/canister_lifecycle/post_upgrade.rs
+++ src/canister/individual_user_template/src/api/canister_lifecycle/post_upgrade.rs
@@ -68,8 +68,8 @@ fn save_upgrade_args_to_memory() {
 fn migrate_excessive_tokens(){
     CANISTER_DATA.with(|canister_data_ref_cell| {
         let mut canister_data_ref_cell = canister_data_ref_cell.borrow_mut();
-        if canister_data_ref_cell.utility_token_balance > 18_00_00_00_00_00_00_00_00_00 {
-            canister_data_ref_cell.utility_token_balance = 1000;
+        if canister_data_ref_cell.my_token_balance.utility_token_balance > 18_00_00_00_00_00_00_00_00_00 {
+            canister_data_ref_cell.my_token_balance.utility_token_balance = 1000;
         }
     });
 }
