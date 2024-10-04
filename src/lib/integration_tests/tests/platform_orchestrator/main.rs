7a8eefef0e3423d826fda74e24934bed732a6848
Ravi Sawlani <152961362+ravibazz@users.noreply.github.com>
2024-10-04 18:12:21 +0530

    feat: added register and deregister subnet orchestrator methods in platform… (#413)
    added register and deregister subnet orchestrator methods in platform orchestrator

--- src/lib/integration_tests/tests/platform_orchestrator/main.rs
+++ src/lib/integration_tests/tests/platform_orchestrator/main.rs
@@ -1,4 +1,5 @@
 pub mod known_principal;
 pub mod provision_subnet_orchestrator_test;
 pub mod recharge_subnet_orchestrator_test;
+pub mod register_and_deregister_new_subnet_orchestrator_test;
 pub mod update_canisters_last_access_time_test;
