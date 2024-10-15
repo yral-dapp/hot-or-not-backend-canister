9ebef913422d1247bf5913af639ef57b7432402b
Ravi Sawlani <152961362+ravibazz@users.noreply.github.com>
2024-10-15 17:16:23 +0530

    feat: added method to allot empty canisters to individual user canisters (#419)

---
+++ src/canister/user_index/src/api/canister_management/allot_empty_canister.rs
@@ -0,0 +1,39 @@
+use candid::Principal;
+use ic_cdk::caller;
+use ic_cdk_macros::update;
+use shared_utils::constant::{
+    get_backup_individual_user_canister_batch_size, get_backup_individual_user_canister_threshold,
+};
+
+use crate::{
+    util::{
+        canister_management::provision_number_of_empty_canisters,
+        types::individual_user_canister::IndividualUserCanister,
+    },
+    CANISTER_DATA,
+};
+
+#[update]
+fn allot_empty_canister() -> Result<Principal, String> {
+    let registered_individual_canister = IndividualUserCanister::new(caller())?;
+    let result = registered_individual_canister.allot_empty_canister();
+
+    let backup_canister_count =
+        CANISTER_DATA.with_borrow(|canister_data| canister_data.backup_canister_pool.len() as u64);
+
+    if backup_canister_count < get_backup_individual_user_canister_threshold() {
+        let number_of_canisters = get_backup_individual_user_canister_batch_size();
+        let breaking_condition = || {
+            CANISTER_DATA.with_borrow_mut(|canister_data| {
+                canister_data.backup_canister_pool.len() as u64
+                    > get_backup_individual_user_canister_batch_size()
+            })
+        };
+        ic_cdk::spawn(provision_number_of_empty_canisters(
+            number_of_canisters,
+            breaking_condition,
+        ));
+    }
+
+    result
+}
