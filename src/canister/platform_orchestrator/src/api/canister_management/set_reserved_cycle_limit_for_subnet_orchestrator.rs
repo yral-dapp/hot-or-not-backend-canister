cea2d48cc91ad2a349100c7c92afa1573364f4f9
ravibazz <ravi@gobazzinga.io>
2024-10-18 18:54:24 +0530

    added method to update reserved cycles limit for subnet orchestrator

---
+++ src/canister/platform_orchestrator/src/api/canister_management/set_reserved_cycle_limit_for_subnet_orchestrator.rs
@@ -0,0 +1,19 @@
+use candid::Principal;
+use ic_cdk_macros::update;
+
+use crate::{
+    guard::is_caller::is_caller_global_admin_or_controller,
+    utils::registered_subnet_orchestrator::RegisteredSubnetOrchestrator,
+};
+
+#[update(guard = "is_caller_global_admin_or_controller")]
+async fn set_reserved_cycle_limit_for_subnet_orchestrator(
+    subnet_orchestrator_canister_id: Principal,
+    amount: u128,
+) -> Result<(), String> {
+    let registered_subnet_orchestrator =
+        RegisteredSubnetOrchestrator::new(subnet_orchestrator_canister_id)?;
+    registered_subnet_orchestrator
+        .set_reserved_cycle_limit(amount)
+        .await
+}
