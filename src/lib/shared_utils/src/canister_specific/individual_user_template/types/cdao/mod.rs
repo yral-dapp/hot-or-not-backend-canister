146674d0c21955a71114deee59397020efdd5f51
JoeruCodes <medicalajoel@gmail.com>
2024-10-29 11:35:46 +0530

    fix

--- src/lib/shared_utils/src/canister_specific/individual_user_template/types/cdao/mod.rs
+++ src/lib/shared_utils/src/canister_specific/individual_user_template/types/cdao/mod.rs
@@ -10,10 +10,11 @@ pub struct DeployedCdaoCanisters {
     pub root: Principal,
     pub swap: Principal,
     pub index: Principal,
+    #[serde(default)]
     pub airdrop_info: AirdropInfo,
 }
 
-#[derive(CandidType, PartialEq, Eq, Debug, Serialize, Deserialize, Clone)]
+#[derive(CandidType, PartialEq, Eq, Debug, Serialize, Deserialize, Clone, Default)]
 pub struct AirdropInfo {
     /// Maps each principal to their claim status
     pub principals_who_successfully_claimed: HashMap<Principal, ClaimStatus>,
