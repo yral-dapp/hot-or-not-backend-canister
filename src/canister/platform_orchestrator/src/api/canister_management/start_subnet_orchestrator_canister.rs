7ca0d281165b36af2fcc8a729c460ddc9713d37d
Ravi Sawlani <152961362+ravi-sawlani-yral@users.noreply.github.com>
2024-10-30 11:13:40 +0530

    fix: avoid leaving canisters in stopped state if an upgrade fails (#439)

---
+++ src/canister/platform_orchestrator/src/api/canister_management/start_subnet_orchestrator_canister.rs
@@ -0,0 +1,14 @@
+use crate::{
+    guard::is_caller::is_caller_global_admin_or_controller,
+    utils::registered_subnet_orchestrator::RegisteredSubnetOrchestrator,
+};
+use candid::Principal;
+use ic_cdk_macros::update;
+
+#[update(guard = "is_caller_global_admin_or_controller")]
+async fn start_subnet_orchestrator_canister(
+    subnet_orchestrator_canister_id: Principal,
+) -> Result<(), String> {
+    let subnet_orchestrator = RegisteredSubnetOrchestrator::new(subnet_orchestrator_canister_id)?;
+    subnet_orchestrator.start_canister().await
+}
