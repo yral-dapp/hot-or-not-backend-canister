91522663d1cbf15f291b2c9810e8d4fb7d694188
Ravi Sawlani <152961362+ravi-sawlani-yral@users.noreply.github.com>
2024-11-13 17:22:24 +0530

    feat: avoid deploying new backup canisters (#456)
    * avoid deploying new backup canisters
    
    * fix test
    
    * fix tests
    
    * fix test
    
    * fix test

--- src/lib/integration_tests/tests/subnet_orchestrator/test_mark_subnet_orchestrator_as_maxed_out_once_backup_canisters_are_consumed.rs
+++ src/lib/integration_tests/tests/subnet_orchestrator/test_mark_subnet_orchestrator_as_maxed_out_once_backup_canisters_are_consumed.rs
@@ -11,9 +11,8 @@ use test_utils::setup::{
     env::pocket_ic_env::get_new_pocket_ic_env, test_constants::get_mock_user_charlie_principal_id,
 };
 
-#[ignore = "we are not provisioning new backup canisters anymore"]
 #[test]
-pub fn provision_new_available_and_backup_canisters_on_signup_if_required() {
+fn test_mark_subnet_orchestrator_as_maxed_out_once_backup_canisters_are_consumed() {
     let (pocket_ic, known_principal) = get_new_pocket_ic_env();
     let platform_canister_id = known_principal
         .get(&KnownPrincipalType::CanisterIdPlatformOrchestrator)
@@ -90,11 +89,11 @@ pub fn provision_new_available_and_backup_canisters_on_signup_if_required() {
             candid::encode_one(()).unwrap(),
         )
         .map(|res| {
-            let available_capacity: u64 = match res {
+            let backup_capacity: u64 = match res {
                 WasmResult::Reply(payload) => candid::decode_one(&payload).unwrap(),
-                _ => panic!("get subnet available capacity call failed"),
+                _ => panic!("get subnet backup capacity call failed"),
             };
-            available_capacity
+            backup_capacity
         })
         .unwrap();
 
@@ -164,13 +163,64 @@ pub fn provision_new_available_and_backup_canisters_on_signup_if_required() {
         })
         .unwrap();
 
-    assert!(subnet_available_canister_cnt > 0);
+    assert_eq!(
+        subnet_available_canister_cnt,
+        TEST_BACKUP_INDIVIDUAL_USER_CANISTER_BATCH_SIZE
+    );
 
-    let subnet_backup_canister_cnt = pocket_ic
+    for i in TEST_INDIVIDUAL_USER_CANISTER_SUBNET_BATCH_SIZE
+        ..(TEST_INDIVIDUAL_USER_CANISTER_SUBNET_BATCH_SIZE + 10)
+    {
+        let _individual_canister_id = pocket_ic
+            .update_call(
+                subnet_orchestrator_canister_id,
+                Principal::self_authenticating((i + 1).to_ne_bytes()),
+                "get_requester_principals_canister_id_create_if_not_exists",
+                encode_one(()).unwrap(),
+            )
+            .map(|reply_payload| {
+                let result: Result<Principal, String> = match reply_payload {
+                    WasmResult::Reply(payload) => candid::decode_one(&payload).unwrap(),
+                    _ => panic!(
+                        "\n🛑 get_requester_principals_canister_id_create_if_not_exists failed\n"
+                    ),
+                };
+                result
+            })
+            .unwrap()
+            .unwrap();
+    }
+
+    for _ in 0..5 {
+        pocket_ic.tick();
+    }
+
+    let subnet_orchestrator_with_capacity_left = pocket_ic
+        .query_call(
+            platform_canister_id,
+            Principal::anonymous(),
+            "get_all_available_subnet_orchestrators",
+            candid::encode_one(()).unwrap(),
+        )
+        .map(|wasm_result| {
+            let result: Vec<Principal> = match wasm_result {
+                WasmResult::Reply(payload) => candid::decode_one(&payload).unwrap(),
+                WasmResult::Reject(err) => panic!(
+                    "\n call to get all available subnet orchestrators failed {} \n",
+                    err
+                ),
+            };
+            result
+        })
+        .unwrap();
+
+    assert!(!subnet_orchestrator_with_capacity_left.contains(&subnet_orchestrator_canister_id));
+
+    let subnet_available_canister_cnt = pocket_ic
         .query_call(
             subnet_orchestrator_canister_id,
             Principal::anonymous(),
-            "get_subnet_backup_capacity",
+            "get_subnet_available_capacity",
             candid::encode_one(()).unwrap(),
         )
         .map(|res| {
@@ -182,5 +232,23 @@ pub fn provision_new_available_and_backup_canisters_on_signup_if_required() {
         })
         .unwrap();
 
-    assert!(subnet_backup_canister_cnt > 0);
+    assert_eq!(subnet_available_canister_cnt, 0);
+
+    let subnet_backup_canister_cnt = pocket_ic
+        .query_call(
+            subnet_orchestrator_canister_id,
+            Principal::anonymous(),
+            "get_subnet_backup_capacity",
+            candid::encode_one(()).unwrap(),
+        )
+        .map(|res| {
+            let backup_capacity: u64 = match res {
+                WasmResult::Reply(payload) => candid::decode_one(&payload).unwrap(),
+                _ => panic!("get subnet backup capacity call failed"),
+            };
+            backup_capacity
+        })
+        .unwrap();
+
+    assert_eq!(subnet_backup_canister_cnt, 0);
 }
