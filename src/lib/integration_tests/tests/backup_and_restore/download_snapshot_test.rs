2fc43f3f8035e3462ce2c88c33d5f59fa91d1c9e
joel-medicala-yral <joel@gobazzinga.io>
2024-12-04 22:32:35 +0530

    added testing

--- src/lib/integration_tests/tests/backup_and_restore/download_snapshot_test.rs
+++ src/lib/integration_tests/tests/backup_and_restore/download_snapshot_test.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, time::Duration};
 
-use candid::{encode_one, Principal};
+use candid::{encode_one, Decode, Encode, Principal};
 use pocket_ic::{PocketIc, WasmResult};
 use shared_utils::{
     canister_specific::{
@@ -14,24 +14,23 @@ use shared_utils::{
             hot_or_not::{BetDirection, BettingStatus, PlacedBetDetail},
             post::{PostDetailsForFrontend, PostDetailsFromFrontend},
             profile::UserProfileDetailsForFrontend,
-        },
-        post_cache::types::arg::PostCacheInitArgs,
+        }, platform_orchestrator::types::args::PlatformOrchestratorInitArgs, post_cache::types::arg::PostCacheInitArgs
     },
     common::types::{known_principal::KnownPrincipalType, utility_token::token_event::TokenEvent},
     constant::RECLAIM_CANISTER_PRINCIPAL_ID,
     types::canister_specific::individual_user_template::error_types::GetUserUtilityTokenTransactionHistoryError,
 };
-use test_utils::setup::test_constants::{
+use test_utils::setup::{env::pocket_ic_env::get_new_pocket_ic_env, test_constants::{
     get_mock_user_alice_principal_id, get_mock_user_bob_principal_id,
     get_mock_user_charlie_principal_id, get_mock_user_dan_principal_id,
-};
+}};
 
 const INDIVIDUAL_TEMPLATE_WASM_PATH: &str =
     "../../../target/wasm32-unknown-unknown/release/individual_user_template.wasm.gz";
 const POST_CACHE_WASM_PATH: &str =
     "../../../target/wasm32-unknown-unknown/release/post_cache.wasm.gz";
 
-#[cfg(feature = "bet_details_heap_to_stable_mem_upgrade")]
+// #[cfg(feature = "bet_details_heap_to_stable_mem_upgrade")]
 #[test]
 fn download_snapshot_test() {
     let pic = PocketIc::new();
@@ -1015,7 +1014,7 @@ fn download_snapshot_test() {
             alice2_individual_template_canister_id,
             reclaim_principal_id,
             "load_snapshot",
-            candid::encode_args((alice_snap_len as u64,)).unwrap(),
+            candid::encode_args(()).unwrap(),
         )
         .map(|reply_payload| {
             let payload: _ = match reply_payload {
@@ -1031,7 +1030,7 @@ fn download_snapshot_test() {
             bob2_individual_template_canister_id,
             reclaim_principal_id,
             "load_snapshot",
-            candid::encode_args((bob_snap_len as u64,)).unwrap(),
+            candid::encode_args(()).unwrap(),
         )
         .map(|reply_payload| {
             let payload: _ = match reply_payload {
@@ -1047,7 +1046,7 @@ fn download_snapshot_test() {
             dan2_individual_template_canister_id,
             reclaim_principal_id,
             "load_snapshot",
-            candid::encode_args((dan_snap_len as u64,)).unwrap(),
+            candid::encode_args(()).unwrap(),
         )
         .map(|reply_payload| {
             let payload: _ = match reply_payload {
@@ -1251,4 +1250,175 @@ fn individual_template_canister_wasm() -> Vec<u8> {
 
 fn post_cache_canister_wasm() -> Vec<u8> {
     std::fs::read(POST_CACHE_WASM_PATH).unwrap()
+}
+
+#[test]
+fn all_canister_snapshot_tests(){
+    let (pocket_ic, known_principal) = get_new_pocket_ic_env();
+    let platform_canister_id = known_principal
+        .get(&KnownPrincipalType::CanisterIdPlatformOrchestrator)
+        .cloned()
+        .unwrap();
+
+    let super_admin = known_principal
+        .get(&KnownPrincipalType::UserIdGlobalSuperAdmin)
+        .cloned()
+        .unwrap();
+
+    let application_subnets = pocket_ic.topology().get_app_subnets();
+
+    let charlie_global_admin = get_mock_user_charlie_principal_id();
+
+    pocket_ic
+        .update_call(
+            platform_canister_id,
+            super_admin,
+            "add_principal_as_global_admin",
+            candid::encode_one(charlie_global_admin).unwrap(),
+        )
+        .unwrap();
+
+    let user_index_canister_id: Principal = pocket_ic
+        .update_call(
+            platform_canister_id,
+            super_admin,
+            "provision_subnet_orchestrator_canister",
+            candid::encode_one(application_subnets[1]).unwrap(),
+        )
+        .map(|res| {
+            let canister_id_result: Result<Principal, String> = match res {
+                WasmResult::Reply(payload) => candid::decode_one(&payload).unwrap(),
+                _ => panic!("Canister call failed"),
+            };
+            canister_id_result.unwrap()
+        })
+        .unwrap();
+
+    for i in 0..50 {
+        pocket_ic.tick();
+    }
+
+    // upgrade pf_orch
+
+    let platform_orchestrator_init_args = PlatformOrchestratorInitArgs {
+        version: "v1.0.0".into(),
+    };
+    pocket_ic
+        .upgrade_canister(
+            platform_canister_id,
+            pf_orch_canister_wasm(),
+            candid::encode_one(platform_orchestrator_init_args).unwrap(),
+            Some(super_admin),
+        )
+        .unwrap();
+    for i in 0..20 {
+        pocket_ic.tick();
+    }
+
+    let reclaim_principal_id = Principal::from_text(RECLAIM_CANISTER_PRINCIPAL_ID).unwrap();
+    
+    let response = pocket_ic.update_call(platform_canister_id, reclaim_principal_id, "save_snapshot_json", Encode!().unwrap()).unwrap();
+    let snapshot_len: u32 = match response{
+        WasmResult::Reply(payload) => candid::decode_one(&payload).unwrap(),
+        _ => panic!(
+            "\n🛑 get_requester_principals_canister_id_create_if_not_exists failed\n"
+        ),
+        _ => panic!(
+            "\n🛑 get_requester_principals_canister_id_create_if_not_exists failed\n"
+        ),
+    };
+
+    let mut data: Vec<u8> = Vec::new();
+    let mut offset: u64 = 0;
+    let chunk_size = 100_000;
+
+    while offset < snapshot_len as u64{
+        let length = std::cmp::min(chunk_size, snapshot_len as u64 - offset);
+
+        let response = pocket_ic.query_call(platform_canister_id, reclaim_principal_id, "download_snapshot", Encode!(&offset, &length).unwrap()).unwrap();
+        let chunk: Vec<u8> = match response{
+            WasmResult::Reply(payload) => candid::decode_one(&payload).unwrap(),
+            _ => panic!(
+                "\n🛑 get_requester_principals_canister_id_create_if_not_exists failed\n"
+            ),
+            _ => panic!(
+                "\n🛑 get_requester_principals_canister_id_create_if_not_exists failed\n"
+            ),
+        };
+
+        data.extend_from_slice(&chunk);
+        offset += length;
+    }
+
+    println!("data: {}", serde_json::from_slice::<'_, String>(&data).unwrap());
+
+    let snapshot_len = data.len() as u64;
+    let mut offset: u64 = 0;
+    let chunk_size: u64 = 100_000;
+
+    while offset < snapshot_len{
+        let length = std::cmp::min(chunk_size, snapshot_len - offset);
+        let chunk = &data[(offset as usize)..((offset + length) as usize)];
+
+        pocket_ic.update_call(platform_canister_id, reclaim_principal_id, "recieve_and_save_snapshot", Encode!(&offset, &chunk).unwrap()).unwrap();
+        offset += length;
+    }
+
+    pocket_ic.update_call(platform_canister_id, reclaim_principal_id, "load_snapshot", Encode!(&()).unwrap()).unwrap();
+
+    let response = pocket_ic.update_call(user_index_canister_id, reclaim_principal_id, "save_snapshot_json", Encode!().unwrap()).unwrap();
+    let snapshot_len: u32 = match response{
+        WasmResult::Reply(payload) => candid::decode_one(&payload).unwrap(),
+        _ => panic!(
+            "\n🛑 get_requester_principals_canister_id_create_if_not_exists failed\n"
+        ),
+        _ => panic!(
+            "\n🛑 get_requester_principals_canister_id_create_if_not_exists failed\n"
+        ),
+    };
+
+    let mut data: Vec<u8> = Vec::new();
+    let mut offset: u64 = 0;
+    let chunk_size = 100_000;
+
+    while offset < snapshot_len as u64{
+        let length = std::cmp::min(chunk_size, snapshot_len as u64 - offset);
+
+        let response = pocket_ic.query_call(user_index_canister_id, reclaim_principal_id, "download_snapshot", Encode!(&offset, &length).unwrap()).unwrap();
+        let chunk: Vec<u8> = match response{
+            WasmResult::Reply(payload) => candid::decode_one(&payload).unwrap(),
+            _ => panic!(
+                "\n🛑 get_requester_principals_canister_id_create_if_not_exists failed\n"
+            ),
+            _ => panic!(
+                "\n🛑 get_requester_principals_canister_id_create_if_not_exists failed\n"
+            ),
+        };
+
+        data.extend_from_slice(&chunk);
+        offset += length;
+    }
+
+    println!("data: {}", serde_json::from_slice::<'_, String>(&data).unwrap());
+
+    let snapshot_len = data.len() as u64;
+    let mut offset: u64 = 0;
+    let chunk_size: u64 = 100_000;
+
+    while offset < snapshot_len{
+        let length = std::cmp::min(chunk_size, snapshot_len - offset);
+        let chunk = &data[(offset as usize)..((offset + length) as usize)];
+
+        pocket_ic.update_call(user_index_canister_id, reclaim_principal_id, "recieve_and_save_snapshot", Encode!(&offset, &chunk).unwrap()).unwrap();
+        offset += length;
+    }
+
+    pocket_ic.update_call(user_index_canister_id, reclaim_principal_id, "load_snapshot", Encode!(&()).unwrap()).unwrap();
+}
+
+const PF_ORCH_WASM_PATH: &str =
+    "../../../target/wasm32-unknown-unknown/release/platform_orchestrator.wasm.gz";
+
+fn pf_orch_canister_wasm() -> Vec<u8> {
+    std::fs::read(PF_ORCH_WASM_PATH).unwrap()
 }
