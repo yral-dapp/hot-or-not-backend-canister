1f23696be30097315bafdda7f8c00c205c754013
Ravi Sawlani <152961362+ravi-sawlani-yral@users.noreply.github.com>
2024-12-18 11:21:26 +0530

    feat: bump version for sns canisters (#483)

--- wasms/fetch_sns_canisters.sh
+++ wasms/fetch_sns_canisters.sh
@@ -1,7 +1,16 @@
-DFX_IC_COMMIT=e54d3fa34ded227c885d04e64505fa4b5d564743
-CANISTER_NAME=ic-icrc1-index-ng.wasm.gz
+DFX_IC_COMMIT=aa91ecacdf3824e193e21b70e0127e8d3edab51a
+CANISTER_NAME=sns-root-canister.wasm.gz
 
 if [ ! -e "./wams/$CANISTER_NAME" ]; then
     wget -P ./wasms/ "https://download.dfinity.systems/ic/$DFX_IC_COMMIT/canisters/$CANISTER_NAME"
 fi
+
+# archive canister commit: e54d3fa34ded227c885d04e64505fa4b5d564743
+# ledger canister commit: e54d3fa34ded227c885d04e64505fa4b5d564743
+# swap canister commit: aa91ecacdf3824e193e21b70e0127e8d3edab51a
+# governance canister commit: 25c1bb0227d9970f5673b908817d7c4962b29911
+# root canister commit: aa91ecacdf3824e193e21b70e0127e8d3edab51a
+# index canister commit: e54d3fa34ded227c885d04e64505fa4b5d564743
+
+
 
