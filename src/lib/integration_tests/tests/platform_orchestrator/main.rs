e4a03fc6180d6e987dcc6f3d193ba655206380a7
Ravi Sawlani <152961362+ravi-sawlani-yral@users.noreply.github.com>
2025-02-27 12:11:23 +0530

    feat: upgrade a subnet orchestrator with latest wasm (#522)

--- src/lib/integration_tests/tests/platform_orchestrator/main.rs
+++ src/lib/integration_tests/tests/platform_orchestrator/main.rs
@@ -5,3 +5,4 @@ pub mod recharge_subnet_orchestrator_test;
 pub mod register_and_deregister_new_subnet_orchestrator_test;
 pub mod reset_ml_feed_cache_test;
 pub mod update_canisters_last_access_time_test;
+pub mod test_upgrade_subnet_orchestrator_canister_with_latest_wasm;
