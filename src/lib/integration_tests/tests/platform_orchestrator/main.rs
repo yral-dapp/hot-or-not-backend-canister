5327d65d06e295e410e2d7ce618af3214eaa6cf2
ravi-sawlani-yral <ravi@gobazzinga.io>
2025-04-07 18:12:21 +0530

    update candid and removed tests

--- src/lib/integration_tests/tests/platform_orchestrator/main.rs
+++ src/lib/integration_tests/tests/platform_orchestrator/main.rs
@@ -3,6 +3,5 @@ pub mod provision_empty_canisters_in_a_subnet_test;
 pub mod provision_subnet_orchestrator_test;
 pub mod recharge_subnet_orchestrator_test;
 pub mod register_and_deregister_new_subnet_orchestrator_test;
-pub mod reset_ml_feed_cache_test;
-pub mod update_canisters_last_access_time_test;
 pub mod test_upgrade_subnet_orchestrator_canister_with_latest_wasm;
+pub mod update_canisters_last_access_time_test;
