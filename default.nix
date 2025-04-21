ad60290078dd293efdeefec32a033ded77a92bed
Saikat Das <saikatdas0790@gmail.com>
2025-04-21 08:38:29 +0000

    feat: update Nix configuration for flake support and improve development environment

--- default.nix
+++ default.nix
@@ -1,21 +1,47 @@
-{}:
+{ }:
 let
   rev = "1c3a28d84f970e7774af04372ade06399add182e";
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
   pkgs = import nixpkgs { };
-  dfx-env = import (builtins.fetchTarball "https://github.com/ninegua/ic-nix/releases/download/20240610/dfx-env.tar.gz") { version = "20240610"; inherit pkgs; };
+  dfx-env =
+    import
+      (builtins.fetchTarball "https://github.com/ninegua/ic-nix/releases/download/20240610/dfx-env.tar.gz")
+      {
+        version = "20240610";
+        inherit pkgs;
+      };
 in
 dfx-env.overrideAttrs (old: {
-  nativeBuildInputs = with pkgs; old.nativeBuildInputs ++
-    [
-      rustup pkg-config openssl protobuf cmake cachix killall jq coreutils bc python3Full libiconv wget
-    ] ++ (if pkgs.stdenv.isDarwin then [
-      darwin.apple_sdk.frameworks.Foundation
-      pkgs.darwin.libiconv
-    ] else []);
-    shellHook = ''
-      cargo install --root $out --force candid-extractor
-      ln -s $out/bin/candid-extractor $out/bin/candid-extractor
-      export PATH="$out/bin:$PATH"
-    '';
+  nativeBuildInputs =
+    with pkgs;
+    old.nativeBuildInputs
+    ++ [
+      rustup
+      pkg-config
+      openssl
+      protobuf
+      cmake
+      cachix
+      killall
+      jq
+      coreutils
+      bc
+      python3Full
+      libiconv
+      wget
+    ]
+    ++ (
+      if pkgs.stdenv.isDarwin then
+        [
+          darwin.apple_sdk.frameworks.Foundation
+          pkgs.darwin.libiconv
+        ]
+      else
+        [ ]
+    );
+  shellHook = ''
+    cargo install --root $out --force candid-extractor
+    ln -s $out/bin/candid-extractor $out/bin/candid-extractor
+    export PATH="$out/bin:$PATH"
+  '';
 })
--- flake.nix
+++ flake.nix
@@ -1,62 +1,57 @@
 {
-  description = "Rust development environment";
+  description = "Flake for hot-or-not-backend-canister";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/1c3a28d84f970e7774af04372ade06399add182e";
+    dfx-env = {
+      url = "github:ninegua/ic-nix";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        # Read the file relative to the flake's root
-        overrides = (builtins.fromTOML (builtins.readFile (self + "/rust-toolchain.toml")));
-        libPath = with pkgs; lib.makeLibraryPath [
-          # load external libraries that you need in your rust project here
-        ];
-      in
-      {
-        devShells.default = pkgs.mkShell rec {
-          nativeBuildInputs = [ pkgs.pkg-config ];
-          buildInputs = with pkgs; [
-            clang
-            llvmPackages.bintools
+  outputs =
+    {
+      self,
+      nixpkgs,
+      dfx-env,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShell.${system} = pkgs.mkShell {
+        nativeBuildInputs =
+          with pkgs;
+          [
             rustup
-          ];
-
-          RUSTC_VERSION = overrides.toolchain.channel;
-          
-          # https://github.com/rust-lang/rust-bindgen#environment-variables
-          LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
-          
-          shellHook = ''
-            export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
-            export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
-          '';
-
-          # Add precompiled library to rustc search path
-          RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
-            # add libraries here (e.g. pkgs.libvmi)
-          ]);
-          
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (buildInputs ++ nativeBuildInputs);
-
-          
-          # Add glibc, clang, glib, and other headers to bindgen search path
-          BINDGEN_EXTRA_CLANG_ARGS =
-          # Includes normal include path
-          (builtins.map (a: ''-I"${a}/include"'') [
-            # add dev libraries here (e.g. pkgs.libvmi.dev)
-            pkgs.glibc.dev
-          ])
-          # Includes with special directory paths
-          ++ [
-            ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
-            ''-I"${pkgs.glib.dev}/include/glib-2.0"''
-            ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
-          ];
-        };
-      }
-    );
+            pkg-config
+            openssl
+            protobuf
+            cmake
+            cachix
+            killall
+            jq
+            coreutils
+            bc
+            python3Full
+            libiconv
+            wget
+          ]
+          ++ (
+            if pkgs.stdenv.isDarwin then
+              [
+                darwin.apple_sdk.frameworks.Foundation
+                pkgs.darwin.libiconv
+              ]
+            else
+              [ ]
+          );
+        shellHook = ''
+          cargo install --root $out --force candid-extractor
+          ln -s $out/bin/candid-extractor $out/bin/candid-extractor
+          export PATH="$out/bin:$PATH"
+        '';
+      };
+    };
 }
