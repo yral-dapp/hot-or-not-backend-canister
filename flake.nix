bd1389e2dab9a114b0222619c9cc24879f1e9c50
Saikat Das <saikatdas0790@gmail.com>
2025-04-23 16:41:01 +0000

    refactor: update flake configuration and improve development environment setup

--- flake.nix
+++ flake.nix
@@ -1,45 +1,74 @@
 {
-  description = "Flake for hot-or-not-backend-canister";
+  description = "Hot-or-not backend canister development environment";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/1c3a28d84f970e7774af04372ade06399add182e";
+    flake-utils.url = "github:numtide/flake-utils";
     ic-nix = {
-      url = "github:ninegua/ic-nix/20240610";
+      url = "github:ninegua/ic-nix";
       flake = false;
     };
   };
 
   outputs =
     {
-      self,
       nixpkgs,
+      flake-utils,
       ic-nix,
       ...
     }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      dfx-env = pkgs.callPackage "${ic-nix}/dfx-env.nix" {
-        force = true;
-      };
-    in
-    {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        buildInputs = [
-          dfx-env
-          pkgs.rustup
-          pkgs.pkg-config
-          pkgs.openssl
-          pkgs.protoc
-          pkgs.cmake
-          pkgs.cachix
-          pkgs.killall
-          pkgs.jq
-          pkgs.coreutils
-          pkgs.bc
-          pkgs.python3Full
-          pkgs.libiconv
-          pkgs.wget
-        ] ++ (if pkgs.stdenv.isDarwin then [ pkgs.darwin.libiconv ] else [ ]);
-      };
-    };
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        # Use ic-nix input directly rather than fetchTarball
+        dfx-env = import "${ic-nix}/dfx-env.nix" {
+          inherit pkgs;
+          force = true;
+        };
+      in
+      {
+        devShells.default = dfx-env.overrideAttrs (old: {
+          nativeBuildInputs =
+            with pkgs;
+            old.nativeBuildInputs
+            ++ [
+              rustup
+              pkg-config
+              openssl
+              protobuf
+              cmake
+              cachix
+              killall
+              jq
+              coreutils
+              bc
+              python3Full
+              libiconv
+              wget
+            ]
+            ++ (
+              if pkgs.stdenv.isDarwin then
+                [
+                  darwin.apple_sdk.frameworks.Foundation
+                  darwin.libiconv
+                ]
+              else
+                [ ]
+            );
+
+          shellHook = ''
+            # Ensure the DFX environment is properly set up
+            export DFX_CONFIG_ROOT="$PWD/.dfx/config"
+            export DFX_CACHE_ROOT="/tmp/dfx-cache"
+
+            echo "Installing candid-extractor..."
+            if ! command -v candid-extractor &> /dev/null; then
+              cargo install --quiet candid-extractor
+            fi
+          '';
+        });
+      }
+    );
 }
